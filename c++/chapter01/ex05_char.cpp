bd7db60dfb219e870d6f2d6a8a05cb35762d9aac
yongin <kkc926@naver.com>
2020-09-04 17:54:56 +0900

    my first c++

---
+++ c++/chapter01/ex05_char.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+char ch;
+ch = 'a';
+cout << ch;
+
+
+// ch='ab'; //에러
+// ch="ab"; //에러
+// ch='';// 에러
+
+return 0;
+}
