078833ced73ca98899685a91d97fcae3eb1fc170
yongin <kkc926@naver.com>
2020-09-05 23:24:55 +0900

    study 200905

---
+++ baekjoon/level 1/ex11.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, b,num_1,num_10,num_100;
+	cin >> a >> b;
+
+	num_1 = b % 10;
+	num_10 = (b % 100 - num_1);
+	num_100 = (b - num_10 - num_1);
+
+	cout << a * num_1 << endl;
+	cout <<(a * num_10)/10  <<endl;
+	cout << (a * num_100)/100 << endl;
+	cout << a * num_1+ (a * num_10)+ (a * num_100) << endl;
+
+	return 0;
+}
