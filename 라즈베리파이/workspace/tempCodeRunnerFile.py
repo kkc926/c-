bea20490796d24dfe7de5aa411e9c201ce3a1edc
yongin <kkc926@naver.com>
2020-09-29 17:49:29 +0900

    raspberry

--- 라즈베리파이/workspace/tempCodeRunnerFile.py
+++ 라즈베리파이/workspace/tempCodeRunnerFile.py
@@ -1,1 +1,8 @@
-print("Hello PI")
+from time import sleep
+from picamera import PiCamera
+camera = PiCamera()
+camera.start_preview()
+sleep(2)
+for filename in camera.capture_continuous('img{counter:03d}.jpg'):
+    print('Captured %s' % filename)
+    sleep(10) # wait 10 seconds
